34560b6ebc270a0b000ddc8ad210b1b4d1cc2870
David Jensen <david.lgj@gmail.com>
2012-04-04 15:53:32 +0200

    Version bump

--- lf.js
+++ lf.js
@@ -1,5 +1,22 @@
+//    Copyright: David Jensen 2012 <david.lgj@gmail.com>
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    
 //API described by Björn Sållarp
 //http://blog.sallarp.com/lansforsakringar-api.html
+
 var _ = require('underscore');
 var request = require('request');
 
